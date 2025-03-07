480372ee7124e2093cdda33ef7e78ed785a2f036
Bakuwan <f4dhliraihan@gmail.com>
2025-03-07 19:25:53 +0700

    [REFACTOR] Apply PaymentStatus enum check to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import enums.OrderStatus;
+import enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Arrays;
@@ -30,23 +32,18 @@ public class Payment {
     public Payment(String id, String method, Map<String, String> paymentData, String status) {
         this(id, method, paymentData);
 
-        String[] validStatuses = {"PENDING", "SUCCESS", "REJECTED"};
-        if (Arrays.stream(validStatuses).noneMatch(item -> item.equals(status))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
+        this.setStatus(status);
+
         validatePaymentData();
     }
 
 
     public void setStatus(String status) {
-        String[] validStatuses = {"PENDING", "SUCCESS", "REJECTED"};
-        if (status == null || status.trim().isEmpty()
-                || Arrays.stream(validStatuses).noneMatch(item -> item.equals(status))) {
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
             throw new IllegalArgumentException();
         }
-        this.status = status;
     }
 
     private void validatePaymentData() {
@@ -56,21 +53,21 @@ public class Payment {
         if (method.equalsIgnoreCase("Voucher")) {
             String voucher = paymentData.get("voucherCode");
             if (voucher == null) {
-                this.status = "REJECTED";
+                this.status = PaymentStatus.REJECTED.getValue();
             } else {
                 boolean valid = voucher.length() == 16 &&
                         voucher.startsWith("ESHOP") &&
                         countDigits(voucher) == 8;
-                this.status = valid ? "SUCCESS" : "REJECTED";
+                this.status = valid ? PaymentStatus.SUCCESS.getValue() : PaymentStatus.REJECTED.getValue();
             }
         } else if (method.equalsIgnoreCase("BankTransfer")) {
             String bankName = paymentData.get("bankName");
             String referenceCode = paymentData.get("referenceCode");
             if (bankName == null || bankName.trim().isEmpty() ||
                     referenceCode == null || referenceCode.trim().isEmpty()) {
-                this.status = "REJECTED";
+                this.status = PaymentStatus.REJECTED.getValue();
             } else {
-                this.status = "SUCCESS";
+                this.status = PaymentStatus.SUCCESS.getValue();
             }
         }
     }
